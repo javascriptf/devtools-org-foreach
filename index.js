3ab8908d036ef43dd8ea1cccc947232ab9ace81b
Subhajit Sahu <wolfram77@gmail.com>
2022-09-17 16:28:07 +0530

    :mango: add org badge to puzzlef repos

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cp = require('extra-child-process');
 
 const E = process.env;
 const stdio = [0, 1, 2];
-const org   = 'ifct2017';
+const org   = 'puzzlef';
 const REPOS = [];
 
 
@@ -14,27 +14,31 @@ const REPOS = [];
 async function forEachRepo(options, fn) {
   var o = Object.assign({}, options);
   var octokit = new Octokit({auth: o.auth});
+  var repos   = new Map();
   for (var page=0;; ++page) {
-    var repos = await octokit.repos.listForOrg({org: o.org, page});
-    if (repos.data.length===0) break;
-    for (var r of repos.data)
-      fn(r);
+    var pageRepos = await octokit.repos.listForOrg({org: o.org, page, per_page: 100});
+    if (pageRepos.data.length===0) break;
+    for (var r of pageRepos.data)
+      repos.set(r.name, r);
   }
+  for (var r of repos.values())
+    fn(r);
 }
 
 
 function addOrgBadge() {
-  var badge = `https://img.shields.io/badge/org-${org}-green?logo=Org`;
-  var url   = `https://${org}.github.io`
-  for (var repo of REPOS) {
-    console.log(`Processing ${org}/${repo} ...`);
-    cp.execSync(`rm -rf tmp && git clone https://github.com/${org}/${repo} tmp`, {stdio});
+  var badge   = `https://img.shields.io/badge/org-${org}-green?logo=Org`;
+  var url     = `https://${org}.github.io`
+  var options = {auth: E.GITHUB_TOKEN, org};
+  forEachRepo(options, repo => {
+    console.log(`\nProcessing ${org}/${repo.name} ...`);
+    cp.execSync(`rm -rf tmp && git clone https://github.com/${org}/${repo.name} tmp`, {stdio});
     var md = fs.readFileTextSync('tmp/README.md');
-    if (md.includes('[![ORG]') || !md.includes('[![DOI]')) continue;
+    if (md.includes('[![ORG]') || !md.includes('[![DOI]')) return;
     md = md.replace('[![DOI]', `[![ORG](${badge})](${url})\n[![DOI]`);
     fs.writeFileTextSync('tmp/README.md', md);
     cp.execSync(`enpm push "w/ org badge"`, {stdio, cwd: 'tmp'});
-  }
+  });
 }
 
 
@@ -52,4 +56,4 @@ function updateGitignore() {
     cp.execSync(`egit apush`, {stdio, cwd: 'tmp'});
   })
 }
-updateGitignore();
+addOrgBadge();
